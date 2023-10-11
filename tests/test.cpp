9bb0fc462d245fce7486251faedb8b5cf000e789
Lisa <liza.nickonova@mail.ru>
2023-10-11 16:00:38 +0300

    Добавлены сценарии для функции canAddPiece

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "../2048_game_2023/functions.h"
+#include "gtestbdd.h"
 
 //Модульные тесты для функции isWin
 
@@ -21,4 +22,29 @@ TEST(IsWinTest, DoesNotHave2048) {
 	bool res = isWin(win);
 	//assert
 	EXPECT_EQ(false, res);
+}
+
+//Сценарии для функции canAddPiece
+FEATURE("Pieces can be added on an empty space") {
+	//AS_A player
+	//I_WANT to know if a piece can be added
+	//SO_THAT I know if I can continue playing
+}
+SCENARIO("New piece can be added on a board")
+{
+	GIVEN("A board with an empty space")
+		int board[4][4] = { {2, 2, 2, 2}, {2, 2, 2, 2}, {2, 2, 0, 2}, {2, 2, 2, 2} };
+	WHEN("I made a move")
+		bool res = canAddPiece(board);
+	THEN("The result should be true")
+		ASSERT_EQ(true, res);
+}
+SCENARIO("New piece cannot be added on a board")
+{
+	GIVEN("A board without an empty space")
+		int board[4][4] = { {2, 2, 2, 2}, {2, 2, 2, 2}, {2, 2, 2, 2}, {2, 2, 2, 2} };
+	WHEN("I made a move")
+		bool res = canAddPiece(board);
+	THEN("The result should be false")
+		ASSERT_EQ(false, res);
 }
